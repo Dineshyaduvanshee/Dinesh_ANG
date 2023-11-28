26ac2a9a4cafbda6b9fdf6e81053c2f95d8915d8
Dinesh yadav <dineshyadav74950@gmail.com>
2023-11-28 12:46:55 +0530

    ng show and hide

--- module7.js
+++ module7.js
@@ -1,4 +1,10 @@
 var app = angular.module("MyMod",[])
 app.controller("myController",function($scope){
-    $scope.name="Quantique meta data pvt ltd."
+    $scope.Employee = [
+        {name:"Dinesh",designation:"Devoloper",age:23,salary:230000},
+        {name:"Mahesh",designation:"Doctor",age:20,salary:340000000},
+        {name:"Ritesh",designation:"pilot",age:18,salary:5000000000},
+        {name:"Nilesh",designation:"polish",age:17,salary:340000000},
+        {name:"Titu",designation:"Enginear",age:12,salary:450000000}
+    ];
 });
